90812f6a559a4ca7ab4efd3c4768d9263fe595bf
naga bharat <naga@synycssolutions.com>
2016-12-24 16:18:04 +0530

    added support

--- src/main/scala-2.11/com/synycs/consumerdata/DataXml.scala
+++ src/main/scala-2.11/com/synycs/consumerdata/DataXml.scala
@@ -1,5 +1,7 @@
 package com.synycs.consumerdata
 
+import java.io.File
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
 
@@ -8,29 +10,35 @@ import org.apache.spark.sql.DataFrame
   */
 object DataXml {
   def main(args: Array[String]) {
+
+
     val spark = SparkSession
       .builder
       .appName("TfIdfExample").master("local")
       .getOrCreate()
 
-//    val rdf=spark.sparkContext.textFile("/home/synycs/scala/proj/Consumerdata.xml").
-//      map(x=>x.replaceAll("<row>","").replaceAll("</row></response>","</response>"))
-//    rdf.saveAsTextFile("/home/synycs/scala/proj/Consumerdata-ref.xml")
+    val rdf=spark.sparkContext.textFile("/home/synycs/scala/proj/Consumerdata.xml").
+      map{x=>x.replaceAll("<row>","").
+        replaceAll("</row>(\\s)*</response>","</response>")
+      }
+    rdf.saveAsTextFile("/home/synycs/scala/proj/Consumerdata-ref.xml")
 
 
 
-        val forSchema=spark.read
-          .format("com.databricks.spark.xml")
-          .option("rootTag", "response")
-          .option("rowTag", "row")
-          //  .option("charset","UTF-8")
-          //      .schema(customSchema)()
-          .load("/home/synycs/scala/proj/Consumerdata-ref.xml/part-00000")
-
-        forSchema.show()
+//        val forSchema=spark.read
+//          .format("com.databricks.spark.xml")
+//          .option("rootTag", "response")
+//          .option("rowTag", "row")
+//          //  .option("charset","UTF-8")
+//          //      .schema(customSchema)()
+//          .load("/home/synycs/scala/proj/Consumerdata-ref.xml/part-00000")
+//
+//        forSchema.show()
 
 
 
   }
+
+
 
 }
