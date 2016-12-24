86a3e7e2b88d300c1736539378429b31418ef898
naga bharat <naga@synycssolutions.com>
2016-12-24 14:51:02 +0530

    added support

---
+++ src/main/scala-2.11/com/synycs/consumerdata/DataXml.scala
@@ -0,0 +1,36 @@
+package com.synycs.consumerdata
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+
+/**
+  * Created by synycs on 24/12/16.
+  */
+object DataXml {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("TfIdfExample").master("local")
+      .getOrCreate()
+
+//    val rdf=spark.sparkContext.textFile("/home/synycs/scala/proj/Consumerdata.xml").
+//      map(x=>x.replaceAll("<row>","").replaceAll("</row></response>","</response>"))
+//    rdf.saveAsTextFile("/home/synycs/scala/proj/Consumerdata-ref.xml")
+
+
+
+        val forSchema=spark.read
+          .format("com.databricks.spark.xml")
+          .option("rootTag", "response")
+          .option("rowTag", "row")
+          //  .option("charset","UTF-8")
+          //      .schema(customSchema)()
+          .load("/home/synycs/scala/proj/Consumerdata-ref.xml/part-00000")
+
+        forSchema.show()
+
+
+
+  }
+
+}
